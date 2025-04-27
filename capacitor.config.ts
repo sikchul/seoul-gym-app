8d7ad55cb27a61b26403539f939b34736e3523d1
sikchul <proocoss@daum.net>
2025-04-28 00:55:16 +0900

    update capacitor settings

---
+++ capacitor.config.ts
@@ -0,0 +1,17 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const isDev = process.env.BUILD_MODE === 'dev';
+
+const config: CapacitorConfig = {
+  appId: 'com.seoul.gym.app',
+  appName: 'seoul-gym-app',
+  webDir: 'out',
+  ...(isDev && {
+    server: {
+      url: 'http://10.0.2.2:3000',
+      cleartext: true,
+    },
+  }),
+};
+
+export default config;
