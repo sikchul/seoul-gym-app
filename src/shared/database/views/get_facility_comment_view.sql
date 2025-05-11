2e28c23da70ffd79fcadb6042ccfe8e0a351efc9
sikchul <proocoss@daum.net>
2025-05-11 20:09:10 +0900

    update sql view

--- src/shared/database/views/get_facility_comment_view.sql
+++ src/shared/database/views/get_facility_comment_view.sql
@@ -5,4 +5,5 @@ SELECT c.*,
        (c.profile_id = auth.uid()) AS is_my_comment
 FROM comments c
 INNER JOIN profiles p ON c.profile_id = p.profile_id
+WHERE c.profile_id = auth.uid()
 ORDER BY c.created_at DESC;
